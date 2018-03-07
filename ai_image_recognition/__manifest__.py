692bd27eb2c3fe404618e0a2d770d2da841d08ac
lxd <lxd@TJYWD.COM>
2018-03-07 10:40:23 +0800

    说明中修改下载源

--- ai_image_recognition/__manifest__.py
+++ ai_image_recognition/__manifest__.py
@@ -13,12 +13,12 @@ Tensorflow Image Recognition
 
 Installation:
 First installation takes a long time, because file will be download automatically. 
-If download failure, you need download 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz', and create a folder 'imagenet' at this module dir, then decompress file in the this folder
+If download failure, you need download 'https://sourceforge.net/projects/ongoodmod/files/inception-2015-12-05.tgz', and create a folder 'imagenet' at this module dir, then decompress file in the this folder
 
 安装：
-模块首次安装需要一定时间，因为有文件会自动下载。
-如果下载失败，则你需要手动下载‘http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz’，并在模块文件夹建立‘imagenet’子文件夹，把文件解压到那里
-https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
+模块首次安装需要一定时间，因为有模型文件会自动下载。
+如果下载失败，则你需要手动下载‘https://sourceforge.net/projects/ongoodmod/files/inception-2015-12-05.tgz’，并在模块文件夹建立‘imagenet’子文件夹，把文件解压到那里
+
 """,
     'author': "OnGood, Jarvis (www.odomod.com)",
     'website': 'https://www.ongood.cn',
